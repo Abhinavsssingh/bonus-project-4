195cd1f0c1ea753f46f0479ea9bfc598f91d5709
Abhinavsssingh <singhabhinav22082000@gmail.com>
2023-02-14 14:14:10 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const route = require('./routes/route')
+
+app.use(express.json())
+
+app.use('/', route)
+app.listen(3000, () => {
+    console.log("server is started in port in 3000")
+})
